04c69411a25993773aa89991aa81e597a3c21cbb
Leif-Erik Kotsch <leifkotsch@users.noreply.github.com>
2017-11-13 18:13:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import requests
+import json
+import os
 
 #DDNS_LIST syntax: [[username,password],[...,...],...] all entrys must be strings
+
+
 DDNS_LIST = []
 DDNS_LOGIN_URL = ""
 CONSOLE_OUTPUT = false
+#JSON_LIST = ""
 
+
+#if(not os.path.isfile(JSON_LIST)):
+#  if(CONSOLE_OUTPUT):
+#    print("JSON-List dosn´t exist. ")
+
+#with open(JSON_LIST) as temp_json_file:
+#  json_data = json.load(temp_json_file)
+#  temp_json_file.close()
+  
+      
+      
 for _temp in DDNS_LIST:
   temp_request = requests.get(DDNS_LOGIN_URL, auth=(_temp[0],_temp[1]))
   if(not temp_request.status_code == "200"):
